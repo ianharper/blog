6f0f7145ec9a808b84fd50ac8cf630badd70608a
Ian Harper <ian@ianbharper.com>
2016-11-03 20:51:42 -0700

    Sorcery auth added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Blog::Application.routes.draw do
   root to: 'articles#index'
   resources :articles
   resources :tags
+  resources :authors
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
